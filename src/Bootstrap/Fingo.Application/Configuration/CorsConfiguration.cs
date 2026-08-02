489807e64af182773bec621f83ea85db7f5d66ec
Mithil Suthar | MERN Stack Developer | BCA Graduate <136597187+Mithil2603@users.noreply.github.com>
2026-08-02 19:24:09 +0530

    feat: restructure Fingo.Api into Fingo.Application and add necessary configurations

---
+++ src/Bootstrap/Fingo.Application/Configuration/CorsConfiguration.cs
@@ -0,0 +1,7 @@
+namespace Fingo.Application.Configuration
+{
+    public class CorsConfiguration
+    {
+        public IList<string> AllowedOrigins { get; set; } = null!;
+    }
+}
--- src/Features/Common/Fingo.Shared/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Fingo.Shared;
-
-public class Class1
-{
-
-}
